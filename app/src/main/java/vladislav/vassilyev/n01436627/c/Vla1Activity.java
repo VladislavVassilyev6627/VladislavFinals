550a2a22619e18a829d48be48b1f9784521be0a7
VladislavVassilyev6627 <n01436627@humbermail.ca>
2022-04-20 18:09:34 -0400

    Initial commit Vladislav Vassileyv n01436627

---
+++ app/src/main/java/vladislav/vassilyev/n01436627/c/Vla1Activity.java
@@ -0,0 +1,14 @@
+package vladislav.vassilyev.n01436627.c;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Vla1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
